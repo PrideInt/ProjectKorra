6ad89257ef82387854aa4093c7b06fbe937d5290
Pride Y <xixipepperd@gmail.com>
2021-03-23 16:47:51 -0400

    Hex color support

--- src/com/projectkorra/projectkorra/util/ActionBar.java
+++ src/com/projectkorra/projectkorra/util/ActionBar.java
@@ -2,6 +2,7 @@ package com.projectkorra.projectkorra.util;
 
 import org.bukkit.entity.Player;
 
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 
@@ -10,6 +11,14 @@ public class ActionBar {
 	public static void sendActionBar(final String message, final Player... player) {
 		for (Player e : player) {
 			e.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(message));
+		}
+	}
+	
+	public static void sendActionBar(final String message, ChatColor color, final Player... player) {
+		for (Player e : player) {
+			TextComponent tc = new TextComponent(message);
+			tc.setColor(color);
+			e.spigot().sendMessage(ChatMessageType.ACTION_BAR, tc);
 		}
 	}
 }
